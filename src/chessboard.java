e3bbcd0cfbd12cb29804672b1f2eb72f42d08dc5
Andrew Latham <acl68@case.edu>
2012-05-13 01:23:41 -0400

    Pieces can be captured, but only if they are of opposite sides.  If they are on the same side, the capturing piece just goes back to its original square.

--- src/chessboard.java
+++ src/chessboard.java
@@ -60,30 +60,48 @@ public class chessboard extends JPanel{
 		});
 		addMouseMotionListener(new MouseMotionAdapter(){
 			public void mouseDragged(MouseEvent evt){
-				System.out.println(pickedUp);
 				if (pickedUp){
 					System.out.println(pickedUpPiece.type);
 					if (pickedUpPiece != null){
 						pickedUpPiece.setLocation(evt.getX() - 20, evt.getY() - 20);
-						System.out.println(evt.getX());
 					}
 				}
 			}
 		});
 	}
 	
-	public static void movePiece(Piece piece, int column, int row){
+	public void movePiece(Piece piece, int column, int row){
+		Square oldSquare = piece.square;
 		piece.square.piece = null;
 		piece.square = center.squares[column - 1][row - 1];
-		piece.square.piece = piece;
-		piece.setLocation(piece.square.getPoint());
-		center.frame.repaint();
+		if (piece.square.piece != null){
+			if (piece.square.piece.color == piece.color){
+				piece.square = oldSquare;
+				piece.square.piece = piece;
+				piece.setLocation(piece.square.getPoint());
+			}
+			else
+				capture(piece, piece.square.piece);
+		}
+		else{
+			piece.square.piece = piece;
+			piece.setLocation(piece.square.getPoint());
+			center.frame.repaint();
+		}
 	}
 	
 	public static Square getSquare(Point p){
 		int column = ((int)Math.ceil(((p.x + 1) / 40) + 0.1) - 1);
 		int row = 8 - (int)Math.ceil(((p.y + 1) / 40) + 0.1);
-		System.out.println(p.x + " " + p.y + ": " + column + " " + row);
 		return center.squares[column][row];
+	}
+	
+	public void capture(Piece winner, Piece loser){
+		loser.square.piece = null;
+		loser.square = null;
+		remove(loser);
+		winner.square.piece = winner;
+		winner.setLocation(winner.square.getPoint());
+		center.frame.repaint();
 	}
 }
